656af3ec02491f29f19928af470e22a4f59a23f7
Homin Lee <homin.lee@suapapa.net>
2014-04-16 23:31:01 +0900

    Initial commit

---
+++ namematch.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/suapapa/go_hangul"
+)
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	body, _ := ioutil.ReadFile("main.html")
+	fmt.Fprintf(w, "%s", body)
+}
+
+func matchHandler(w http.ResponseWriter, r *http.Request) {
+	nameMatch(w, r.FormValue("name1"), r.FormValue("name2"))
+}
+
+func nameMatch(w http.ResponseWriter, name1, name2 string) {
+	fmt.Fprintf(w, "<html>%s %s<br>", name1, name2)
+	r1 := []rune(name1)
+	r2 := []rune(name2)
+	if len(r1) != 3 || len(r2) != 3 {
+		http.Error(w, "석 자 이름을 넣으세요", http.StatusBadRequest)
+		return
+	}
+
+	rc := []rune{r1[0], r2[0], r1[1], r2[1], r1[2], r2[2]}
+	rn := make([]int, 6)
+	for i, r := range rc {
+		rn[i] = hangul.Stroke(r)
+	}
+	match(w, rn)
+	fmt.Fprintf(w, "</html>")
+}
+
+func match(w http.ResponseWriter, in []int) {
+	fmt.Fprintf(w, "%v <br>", in)
+	r := make([]int, len(in)-1)
+	for i := 0; i < len(r); i++ {
+		r[i] = (in[i] + in[i+1]) % 10
+	}
+
+	if len(in) > 2 {
+		match(w, r)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/match/", matchHandler)
+
+	http.ListenAndServe(":8080", nil)
+}
